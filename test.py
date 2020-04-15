ce31cec93e9d085789e7a57c1e8805ce245881c1
haotianz <haotianz@stanford.edu>
2020-04-14 17:45:51 -0700

    add split_id

--- test.py
+++ test.py
@@ -57,6 +57,9 @@ if __name__ == '__main__':
     if opt.eval:
         model.eval()
     for i, data in enumerate(dataset):
+        if i % opt.num_split != opt.split_id:
+            continue
+            
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
         model.set_input(data)  # unpack data from data loader
